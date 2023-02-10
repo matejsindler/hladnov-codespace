28ed36df5382fda9ad9bb9528a94f3092e878280
matejsindler <120733832+matejsindler@users.noreply.github.com>
2023-02-10 17:25:12 +0000

    mrizka

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 let had = [document.querySelector(".had")]
 
+const noveTlacitko = document.getElementById("tlacitko")
+noveTlacitko.addEventListener("click", zmenaMrizsky)
+
+function zmenaMrizsky() {
+  const mrizka = document.getElementById("plocha");
+
+  for (const element of mrizka.querySelectorAll("br, .pole")) {
+    element.remove()
+  }
+  const puvodniMezera = document.createElement("br");
+  mrizka.append(puvodniMezera)
+
+  const poleVelikost = document.getElementById("velikost");
+  const velikost = parseInt(poleVelikost.value);
+  console.log("Měním mřížku na velikost " + velikost)
+  for (let noveX = 1; noveX <= velikost; noveX++) {
+    for (let noveY = 1; noveY <= velikost; noveY++) {
+      const novyDiv = document.createElement("div");
+      novyDiv.classList.add("pole");
+      novyDiv.id = noveX + ":" + noveY
+      mrizka.append(novyDiv);
+      console.log("Přidám div s id" + novyDiv.id)
+      mrizka.append(" ");
+    }
+    const noveBr = document.createElement("br");
+    mrizka.append(noveBr);
+  }
+}
+
+
+
 function pohniHadem(dolu, doprava) {
   const hadiHlava = had[0]
   console.log("Had je na " + hadiHlava.id);
@@ -24,23 +55,39 @@ function pohniHadem(dolu, doprava) {
   }
 }
 
-function pohyb(udalost) {
-  if (udalost.which === 37) {
+let posledniKlavesa = 0;
+function autopohyb(udalost) {
+  const jeToPrvniKlavesa = (posledniKlavesa === 0);
+  posledniKlavesa = udalost.which;
+  if (jeToPrvniKlavesa) {
+    pohyb()
+    setInterval(pohyb, 300);
+  }
+  console.log("Posledni klavesa je " + posledniKlavesa);
+}
+
+function pohyb() {
+  if (posledniKlavesa === 37) {
     console.log("Hade, jdi doleva pls");
     pohniHadem(0, -1);
   }
-  if (udalost.which === 38) {
+  if (posledniKlavesa === 38) {
     console.log("Hade, jdi nahoru pls");
     pohniHadem(-1, 0);
   }
-  if (udalost.which === 39) {
+  if (posledniKlavesa === 39) {
     console.log("Hade, jdi doprava pls");
     pohniHadem(0, 1);
   }
-  if (udalost.which === 40) {
+  if (posledniKlavesa === 40) {
     console.log("Hade, jdi dolů pls");
     pohniHadem(1, 0);
   }
 }
 
-document.addEventListener("keydown", pohyb);
+
+
+
+
+
+document.addEventListener("keydown", autopohyb);
